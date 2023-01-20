dab5c88d620251b44a72252fc4473d3b6c0fa271
devintheamateurdevin <devintheamateurdevin@gmail.com>
2023-01-20 15:35:52 +0300

    Adds level to sample data and vocab seeder

--- src/seeders/DatabaseSeeder.ts
+++ src/seeders/DatabaseSeeder.ts
@@ -17,8 +17,8 @@ export class DatabaseSeeder extends Seeder {
             DictionarySeeder,
             CourseSeeder,
             VocabSeeder,
-            LessonSeeder,
-            MeaningSeeder,
+            // LessonSeeder,
+            // MeaningSeeder,
         ]);
     }
 }
--- src/seeders/VocabSeeder.ts
+++ src/seeders/VocabSeeder.ts
@@ -4,28 +4,38 @@ import fs from "fs-extra";
 import {VocabFactory} from "@/src/seeders/factories/VocabFactory.js";
 import {Vocab} from "@/src/models/entities/Vocab.js";
 import {syncIdSequence} from "@/src/seeders/utils.js";
+import {MapLearnerVocab} from "@/src/models/entities/MapLearnerVocab.js";
 
 export class VocabSeeder extends Seeder {
-    static readonly FILE_NAME = "vocabs.json";
+    static readonly VOCABS_FILE_NAME = "vocabs.json";
+    static readonly MAP_LEARNER_VOCABS_FILE_NAME = "map_vocab_learners.json";
 
     async run(em: EntityManager, context: Dictionary): Promise<void> {
-        if (!await fs.exists(`data/${VocabSeeder.FILE_NAME}`))
+        if (!await fs.exists(`data/${VocabSeeder.VOCABS_FILE_NAME}`) || !await fs.exists(`data/${VocabSeeder.MAP_LEARNER_VOCABS_FILE_NAME}`))
             return;
-        const vocabs = await fs.readJSON(`data/${VocabSeeder.FILE_NAME}`)
+        const vocabs: EntityData<Vocab>[] = await fs.readJSON(`data/${VocabSeeder.VOCABS_FILE_NAME}`)
         const vocabFactory = new VocabFactory(em)
 
         process.stdout.write("seeding vocabs...");
-        vocabs.forEach((vocabData: EntityData<Vocab>) => {
+        vocabs.forEach((vocabData) => {
             em.persist(vocabFactory.makeEntity({
                 id: vocabData.id,
                 text: vocabData.text,
                 language: vocabData.language,
                 isPhrase: vocabData.isPhrase,
-                learners: vocabData.learners
+                learners: []
             }))
         })
         await em.flush();
         await syncIdSequence(em, "vocab")
+
+        const learnerMappings: EntityData<MapLearnerVocab>[] = await fs.readJSON(`data/${VocabSeeder.MAP_LEARNER_VOCABS_FILE_NAME}`)
+        await em.insertMany(MapLearnerVocab, learnerMappings.map((mappingData) => ({
+            learner: mappingData.learner,
+            vocab: mappingData.vocab,
+            level: mappingData.level,
+            notes: mappingData.notes
+        })));
         console.log("done");
     }
 }
