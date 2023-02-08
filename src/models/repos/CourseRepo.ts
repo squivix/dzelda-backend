472e8a523769f24ff7fe02c674f7e042a35ab17d
devintheamateurdevin <devintheamateurdevin@gmail.com>
2023-02-08 12:25:28 +0300

    Fixes NEW words not counted in vocabs by level query

--- src/models/repos/CourseRepo.ts
+++ src/models/repos/CourseRepo.ts
@@ -9,25 +9,24 @@ export class CourseRepo extends EntityRepository<Course> {
     async annotateVocabsByLevel(courses: Course[], userId: number) {
         if (courses.length === 0)
             return courses;
-        const query = `SELECT json_object_agg(outq.id, outq.vocab_levels) AS vocab_levels_by_course 
+        const query = `SELECT json_object_agg(outq.id, outq.vocab_levels) AS vocab_levels_by_course
 FROM (SELECT subq.course_id                          AS id,
-             json_object_agg(subq.level, subq.count) AS vocab_levels 
-      FROM (SELECT lesson.course_id, 
-                   mlv2.level, 
-                   COUNT(mlv2.level) 
-            FROM map_lesson_vocab mlv 
-                     LEFT JOIN map_learner_vocab mlv2 on mlv.vocab_id = mlv2.vocab_id AND mlv2.learner_id = ${userId} 
-                     LEFT JOIN lesson on mlv.lesson_id = lesson.id 
-            WHERE mlv2.level IS NOT NULL AND lesson.course_id IN (${courses.map(c=>c.id).join(",")}) 
-            GROUP BY lesson.course_id, mlv2.level 
-            ORDER BY lesson.course_id) AS subq 
+             json_object_agg(COALESCE(subq.level, 0), subq.count) AS vocab_levels
+      FROM (SELECT lesson.course_id,
+                   mlv2.level,
+                   COUNT(*)
+            FROM map_lesson_vocab mlv
+                     LEFT JOIN map_learner_vocab mlv2 on mlv.vocab_id = mlv2.vocab_id AND mlv2.learner_id = ${userId}
+                     LEFT JOIN lesson on mlv.lesson_id = lesson.id
+            WHERE lesson.course_id IN (${courses.map(c=>c.id).join(",")})
+            GROUP BY lesson.course_id, mlv2.level
+            ORDER BY lesson.course_id) AS subq
       GROUP BY subq.course_id) as outq`;
 
         const vocabsLevelsByCourse = (await this.em.execute(query))[0].vocab_levels_by_course;
+        console.log(vocabsLevelsByCourse)
         const defaultCounts = defaultVocabsByLevel();
-        courses.forEach(course => {
-            course.vocabsByLevel = Object.assign({}, defaultCounts, vocabsLevelsByCourse?.[course.id] ?? {});
-        });
+        courses.forEach(course => course.vocabsByLevel = Object.assign({}, defaultCounts, vocabsLevelsByCourse?.[course.id] ?? {}));
         return courses;
     }
 
--- src/models/repos/LessonRepo.ts
+++ src/models/repos/LessonRepo.ts
@@ -9,23 +9,21 @@ export class LessonRepo extends EntityRepository<Lesson> {
         if (lessons.length === 0)
             return lessons;
         const query = `SELECT json_object_agg(outq.id, outq.vocabLevels) AS vocab_levels_by_lesson
-FROM (SELECT subq.lesson_id                          AS id,
-             json_object_agg(subq.level, subq.count) AS vocabLevels
+FROM (SELECT subq.lesson_id                                                   AS id,
+             json_object_agg(COALESCE(subq.level, 0), subq.count) AS vocabLevels
       FROM (SELECT mlv2.level,
-                   COUNT(mlv2.level),
+                   COUNT(*),
                    mlv.lesson_id
             FROM map_lesson_vocab mlv
                      LEFT JOIN map_learner_vocab mlv2 on mlv.vocab_id = mlv2.vocab_id AND mlv2.learner_id = ${userId}
-            WHERE mlv2.level IS NOT NULL AND mlv.lesson_id IN (${lessons.map(l=>l.id).join(",")})
+            WHERE mlv.lesson_id IN (${lessons.map(l=>l.id).join(",")})
             GROUP BY mlv.lesson_id, mlv2.level
             ORDER BY mlv.lesson_id) AS subq
       GROUP BY subq.lesson_id) as outq`;
 
         const vocabsLevelsByLesson = (await this.em.execute(query))[0].vocab_levels_by_lesson;
         const defaultCounts = numericEnumValues(VocabLevel).reduce((a, v) => ({...a, [v]: 0}), {});
-        lessons.forEach(lesson => {
-            lesson.vocabsByLevel = Object.assign({}, defaultCounts, vocabsLevelsByLesson?.[lesson.id] ?? {});
-        });
+        lessons.forEach(lesson => lesson.vocabsByLevel = Object.assign({}, defaultCounts, vocabsLevelsByLesson?.[lesson.id] ?? {}));
         return lessons;
     }
 
