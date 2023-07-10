2c61f5e1c2cbbef831aeebcee4ad5c7cd0e0c286
devintheamateurdevin <devintheamateurdevin@gmail.com>
2023-07-10 15:55:16 +0300

    Moves sample files

--- tests/sample-files/sampleMockFiles.ts
+++
@@ -1,8 +0,0 @@
-{
-    "size": 71490,
-    "filepath": "public/uploads/courses/images/29e8216c97e1ddc5-1688984956229.png",
-    "newFilename": "19e03758becb682b3b939a500.png",
-    "mimetype": "image/png",
-    "mtime": "2023-07-10T10:29:16.232Z",
-    "originalFilename": "lorem-ipsum-69_8KB-1_1ratio.png"
-}
